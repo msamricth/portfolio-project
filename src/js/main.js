6469f1fae646b1072b7094c8ca17ee24b8c1e55c
Emmelia Talarico <emm.talarico@gmail.com>
2022-09-22 20:50:22 -0400

    Bootstrap Mixins + first airtable integration

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,37 @@
-// Import our custom CSS
-import '../scss/style.sass'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+export function main() {
+    var labels = new Vue({
+        el: '#home',
+        data () {
+        return {
+            siteTitle: null,
+            tagLine:null,
+            loading: true,
+            errored: false
+        }
+        },
+        filters: {
+        footerdecimal (value) {
+            return value.toFixed(2)
+        }
+        },
+        mounted () {
+        var app_id = "appfvaCmvdk54pD1l";
+        var app_key = "keyMMHoSzh3H08K5v";
+    
+        axios
+            .get("https://api.airtable.com/v0/appfvaCmvdk54pD1l/Labels?view=Grid&api_key=keyMMHoSzh3H08K5v")
+            .then(response => {
+            this.siteTitle = response.data.records[0].fields.Name;
+            this.tagLine = response.data.records[0].fields.TagLine;
+            document.getElementById("siteTitle").innerHTML = this.siteTitle;
+            $('#siteTitle').removeClass('placeholder');
+            console.log(this.siteTitle);
+        })
+            .catch(error => {
+            console.log(error)
+            this.errored = true
+        })
+            .finally(() => this.loading = false)
+        }
+    });
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,3 +3,8 @@ import { foo } from "@/foo.js";
 foo();
 
 console.log("Hi there!");
+import { main } from "@/main.js";
+
+main();
+import * as bootstrap from 'bootstrap';
+import { createApp } from 'vue';
